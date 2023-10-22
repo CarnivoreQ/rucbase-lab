db489abf395f21c9d91d38f6d5a59bea5eb90a52
CarnivoreQ <128654957+CarnivoreQ@users.noreply.github.com>
2023-10-22 14:10:32 +0800

    diskmanager_test_1

--- src/storage/disk_manager.cpp
+++ src/storage/disk_manager.cpp
@@ -8,7 +8,7 @@ EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
 MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
 See the Mulan PSL v2 for more details. */
 
-#include "storage/disk_manager.h"
+#include "../storage/disk_manager.h"
 
 #include <assert.h>    // for assert
 #include <string.h>    // for memset
